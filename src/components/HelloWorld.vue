078500bd288fddf7aa76625d27d0fe547f58876e
Wenfang Du <no_reply_mail@163.com>
2023-06-04 14:56:10 +0800

    chore: save

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,25 @@
-<script setup lang="ts">
-  defineProps<{
-    msg: string
-  }>()
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { run } from './utils'
+
+  export default defineComponent({
+    setup() {
+      console.log('setup')
+      run(async () => console.log('run'))
+      Promise.resolve().then(() => console.log('resolved'))
+    },
+    beforeCreate() {
+      console.log('beforeCreate')
+    },
+    mounted() {
+      console.log('mounted')
+    },
+  })
+
+  // Result:
+  // setup
+  // run
+  // beforeCreate
+  // mounted
+  // resolved
 </script>
-
-<template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a>
-      +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>
-      . What's next?
-    </h3>
-  </div>
-</template>
-
-<style scoped>
-  h1 {
-    font-weight: 500;
-    font-size: 2.6rem;
-    top: -10px;
-  }
-
-  h3 {
-    font-size: 1.2rem;
-  }
-
-  .greetings h1,
-  .greetings h3 {
-    text-align: center;
-  }
-
-  @media (min-width: 1024px) {
-    .greetings h1,
-    .greetings h3 {
-      text-align: left;
-    }
-  }
-</style>
---
+++ src/components/utils.ts
@@ -0,0 +1,1 @@
+export const run = (asyncFn: () => Promise<void>) => asyncFn()
