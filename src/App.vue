c4de26fc2b4748f3cd1f78132c61172ada9e8bbc
Wenfang Du <no_reply_mail@163.com>
2023-06-13 13:54:56 +0800

    chore: added repro

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!" foo="" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
